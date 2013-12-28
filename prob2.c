42646a11b3732d64014134809f712bd144f34972
Goaty <duchuy1992@hotmail.com.vn>
2013-12-28 15:43:12 +0900

    Update prob2.c

--- prob2.c
+++ prob2.c
@@ -1,21 +1,26 @@
 #include "stdio.h"
 #include "string.h"
 #include <stdlib.h>
-typedef struct Song{//the Song structure
+typedef struct Song{
 	char name[31];
 	long double quality;
+	int order;
 }Song;
-void swapSong(struct Song* song1, struct Song* song2){//swap between 2 songs
+void swapSong(struct Song* song1, struct Song* song2){
     char stringBuffer[31];
     long double qualityBuffer;
+    int orderBuffer;
     strcpy(stringBuffer,song1->name);
     qualityBuffer=song1->quality;
+    orderBuffer=song1->order;
     strcpy(song1->name,song2->name);
     song1->quality=song2->quality;
+    song1->order=song2->order;
     strcpy(song2->name,stringBuffer);
     song2->quality=qualityBuffer;
+    song2->order=orderBuffer;
 }
-int sortAlbum(struct Song** theAlbum,int length){//receive the pointer to the album as input, sort the album be ascending quality
+int sortAlbumByQuality(struct Song** theAlbum,int length){//sort album by the song quality
     int i,k;
     if(length==1){
 	return 0;
@@ -32,6 +37,36 @@ int sortAlbum(struct Song** theAlbum,int length){//receive the pointer to the al
         }
     }
     return 0;
+}
+int sortAlbumByOrder(struct Song** theAlbum, int length){//after sorting by song Quality, put the song with higher order after the song with lower order (ascending order for songs with same quality)
+    int songNumber;//pointer for going through list of the songs
+    int songPointer;//pointer for going through list of same quality songs
+    int qualityLevel;//specify the quality of song group
+    int memberNumber=0;//Number of members in a song group with same quality
+    int i,k;
+    for (songNumber=1;songNumber<length;songNumber++){
+        qualityLevel=theAlbum[songNumber]->quality;
+        for(songPointer=songNumber;theAlbum[songPointer]->quality=qualityLevel;songPointer++){
+            memberNumber++;
+        }
+        if(memberNumber==1){
+            break;
+        }
+        if(memberNumber==2){
+            if(theAlbum[songNumber]->order>theAlbum[songNumber+1]->order){
+                swapSong(theAlbum[songNumber],theAlbum[songNumber+1]);
+            }
+            memberNumber=1;
+            break;
+        }
+        for(i=songNumber+2;i<songNumber+memberNumber;i++){
+            for(k=i;(theAlbum[k-1]->order>theAlbum[k]->order)&(k>songNumber+1);k--){
+                swapSong(theAlbum[k-1],theAlbum[k]);
+            }
+        }
+        songNumber+=memberNumber;
+        memberNumber=0;
+    }
 }
 int main(){
     struct Song *album[50000];
@@ -58,21 +93,22 @@ int main(){
     selectedNumber=atoi(strtok(NULL, " "));
     for (songNumber=0;songNumber<albumLength;songNumber++){
         fgets(nextLines, 45, stdin);
-	if(nextLines==NULL){
-	    fprintf(stderr,"Not enough input songs.\n");
-	    return 1;
-	}
+        if(nextLines==NULL){
+            fprintf(stderr,"Not enough input songs.\n");
+            return 1;
+        }
         listenTime=atol(strtok(nextLines," ,\n"));
         temp_songName= strtok( NULL ," ,\n");
-	if(temp_songName==NULL){
-	    fprintf(stderr,"Not enough input parameters!\n");
-	    return 1;
-	}
+        if(temp_songName==NULL){
+            fprintf(stderr,"Not enough input parameters!\n");
+            return 1;
+        }
         strcpy(songName,temp_songName);
         songQuality = (listenTime*1.0)/(albumLength-songNumber);
         theSong = (Song*)malloc(sizeof(Song));
         strcpy(theSong->name,songName);
         theSong->quality=songQuality;
+        theSong->order=albumLength-songNumber;
         album[songNumber]=theSong;
     }
     //for testing
@@ -80,10 +116,11 @@ int main(){
     //    printf("Song name: %Lf \n",album[songNumber]->quality);
     //}
     //for testing
-    sortAlbum(album,albumLength);
+    sortAlbumByQuality(album,albumLength);
+    sortAlbumByOrder(album, albumLength);
     if(selectedNumber==0){
-	getchar();
-	return 0;
+        getchar();
+        return 0;
     }
     for (songNumber=albumLength-1;songNumber>=(albumLength -selectedNumber);songNumber--){
         fprintf(stdout,"%s\n",album[songNumber]->name);
@@ -91,7 +128,7 @@ int main(){
     for (songNumber=0;songNumber<albumLength;songNumber++){
         free(album[songNumber]);
     }
- 
     getchar();
     return 0;
 }
+
